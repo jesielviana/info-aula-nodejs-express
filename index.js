48e9da1d62a5959ca62814d263956a2d2d56e84b
Jesiel Viana <jesielviana@proton.me>
2023-10-11 14:04:56 -0300

    organizando o app em camadas e add o prisma

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Express from 'express';
-import usuarios from './usuarios.js';
+import rotasDeUsuarios from './rotas/rotasDeUsuarios.js';
 
 const app = Express();
 app.use(Express.json());
@@ -8,26 +8,7 @@ app.get('/', (req, res) => {
   res.send('Ok!');
 });
 
-app.get('/usuarios', (req, res) => {
-  res.json(usuarios);
-});
-
-app.get('/usuarios/:id', (req, res) => {
-  const id = req.params.id;
-  const usuario = usuarios[id];
-  if (usuario) {
-    res.json(usuario);
-  } else {
-    res.status(404).send('not foud');
-  }
-});
-
-app.post('/usuarios', (req, res) => {
-  const usuario = req.body;
-  console.log('body', usuario);
-  usuarios.push(usuario);
-  res.send('deu certo!');
-});
+app.use(rotasDeUsuarios);
 
 app.listen(3000, () => {
   console.log('app listening on port 3000!');
